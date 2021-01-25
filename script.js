99d84a2a052cd09c781e440d2fc4b673a5f08ae1
timotheemcdonald <timotheemcdonald@gmail.com>
2021-01-24 22:55:29 -0500

    converted global variables to character class object

--- script.js
+++ script.js
@@ -8,14 +8,36 @@ canvas.width = window.innerWidth;
 const images = {};
 images.player = new Image();
 images.player.src = 'cuphead.png';
+const characterActions = ['up', 'top right', 'right', 'down right', 'down', 'jump'];
+const characters = [];
 
-const playerWidth = 103.0625;
-const playerHeight = 113.125;
-let playerFrameX = 3;
-let playerFrameY = 3;
-let playerX = 0;
-let playerY = 0;
-const playerSpeed = 6;
+class Character {
+    constructor(){
+        this.width = 103.0625;
+        this.height = 113.125;
+        this.frameX = 3;
+        this.frameY = 3;
+        this.x = 0;
+        this.y = 0;
+        this.speed = (Math.random() * 1.5) + 3.5;
+        this.action = 'right';
+    }
+    draw(){
+        drawSprite(images.player, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+
+        if(this.frameX < 13) this.frameX++;
+        else this.frameX = 3;
+    }
+    update(){
+        if (this.action === 'right'){
+            if(this.x < canvas.width + this.width) this.x += this.speed;
+            else this.x = 0 - this.width;
+        }
+
+    }
+}
+
+characters.push(new Character());
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
@@ -23,12 +45,8 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight);
-    if (playerFrameX < 13)playerFrameX++;
-    else playerFrameX = 3;
-    //move player
-    if (playerX < canvas.width + playerWidth) playerX += playerSpeed;
-    else playerX = 0 - playerWidth;
+    characters[0].draw();
+    characters[0].update();
 };
 
 window.onload = setInterval(animate, 1000/30);
